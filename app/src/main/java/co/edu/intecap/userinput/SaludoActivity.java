f5bb20c1eb9f365b06546e04b0e19635e2a8a604
julio.saldana <julio.saldana@avantica.com>
2019-04-13 08:19:17 -0500

    update to androidx and add butterknife dependency

--- app/src/main/java/co/edu/intecap/userinput/SaludoActivity.java
+++ app/src/main/java/co/edu/intecap/userinput/SaludoActivity.java
@@ -1,6 +1,9 @@
 package co.edu.intecap.userinput;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.OnClick;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,30 +12,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class SaludoActivity extends AppCompatActivity {
-    private TextView txtSaludo;
-    private EditText editTextNombre;
-    private Button btnSaludar;
+
+    @BindView(R.id.txt_saludo) private TextView txtSaludo;
+    @BindView(R.id.edit_txt_nombre) private EditText editTextNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo); //aqui asignamos el layout
-
-        txtSaludo = findViewById(R.id.txt_saludo);
-        editTextNombre = findViewById(R.id.edit_txt_nombre);
-        btnSaludar = findViewById(R.id.btn_saludar);
-
-        btnSaludar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nombre = editTextNombre.getText().toString();
-                if (!nombre.trim().isEmpty()) {
-                    txtSaludo.setText("Hola " + nombre.trim());
-
-                }else{
-                    Toast.makeText(SaludoActivity.this, "No se aceptan textos vacios", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
+
+
+    @OnClick(R.id.btn_saludar)
+    private  void saludar(View v){
+        String nombre = editTextNombre.getText().toString();
+        if (!nombre.trim().isEmpty()) {
+            txtSaludo.setText("Hello " + nombre.trim());
+
+        }else{
+            Toast.makeText(SaludoActivity.this, "No se aceptan textos vacios", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
